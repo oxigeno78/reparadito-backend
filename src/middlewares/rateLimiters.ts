66f2fef5f7e9df83b2a1d27b19456b91a1aa2606
Ruben Bautista Mendoza <oxigeno78@gmail.com>
2026-02-12 22:05:24 -0600

    Fix: implementación de ipKeyGenerator de rete-limit...

--- src/middlewares/rateLimiters.ts
+++ src/middlewares/rateLimiters.ts
@@ -1,14 +1,17 @@
-import { rateLimit } from "express-rate-limit";
+import { Request } from "express";
+import { rateLimit, ipKeyGenerator } from "express-rate-limit";
 
 export const bookingLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 min
   max: 200,
   standardHeaders: true,
   legacyHeaders: false,
-  keyGenerator: (req) => {
-    const ip = req.ip || "unknown";
-    const userAgent = req.headers["user-agent"] || "unknown";
-    return ip + userAgent;
+  keyGenerator: (req: Request) => {
+    if (req.ip == undefined) {
+      console.error('WARN | `express-rate-limit` | `request.ip` is undefined. You can avoid this by providing a custom `keyGenerator` function, but it may be indicative of a larger issue.');
+    }
+    const ip = req.ip && req.ip.replace(/:\d+[^:]*$/, '') || "unknown"
+    return `${ipKeyGenerator(ip)}:${req.get("user-agent") ?? "unknown"}`;
   }
 });
 
@@ -17,19 +20,23 @@ export const paymentLimiter = rateLimit({
   max: 20,
   standardHeaders: true,
   legacyHeaders: false,
-  keyGenerator: (req) => {
-    const ip = req.ip || "unknown";
-    const userAgent = req.headers["user-agent"] || "unknown";
-    return ip + userAgent;
+  keyGenerator: (req: Request) => {
+    if (req.ip == undefined) {
+      console.error('WARN | `express-rate-limit` | `request.ip` is undefined. You can avoid this by providing a custom `keyGenerator` function, but it may be indicative of a larger issue.');
+    }
+    const ip = req.ip && req.ip.replace(/:\d+[^:]*$/, '') || "unknown";
+    return `${ipKeyGenerator(ip)}:${req.get("user-agent") ?? "unknown"}`;
   }
 });
 
 export const webhookLimiter = rateLimit({
   windowMs: 5 * 60 * 1000,
   max: 200,
-  keyGenerator: (req) => {
-    const ip = req.ip || "unknown";
-    const userAgent = req.headers["user-agent"] || "unknown";
-    return ip + userAgent;
+  keyGenerator: (req: Request) => {
+    if (req.ip == undefined) {
+      console.error('WARN | `express-rate-limit` | `request.ip` is undefined. You can avoid this by providing a custom `keyGenerator` function, but it may be indicative of a larger issue.');
+    }
+    const ip = req.ip && req.ip.replace(/:\d+[^:]*$/, '') || "unknown";
+    return `${ipKeyGenerator(ip)}:${req.get("user-agent") ?? "unknown"}`;
   }
 });
